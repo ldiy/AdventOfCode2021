362ef8978e23bbc6e698102721e2b99f9bca80c5
ldiy <lorenz_reclame@outlook.be>
2021-12-15 23:42:25 +0100

    Day 15: priority queue

--- 15/15.php
+++ 15/15.php
@@ -1,7 +1,8 @@
 <?php
 
-include_once "../tools.php";
+use Ds\PriorityQueue;
 
+include_once "../tools.php";
 const TEST = false;
 //const TEST = true;
 
@@ -19,56 +20,54 @@ function parse_input(): array
     return $parsed;
 }
 
-function find_lowest_total_risk($start_x, $start_y, $stop_x, $stop_y, $map)
+function find_lowest_total_risk_pq($start_x, $start_y, $stop_x, $stop_y, $map)
 {
     $x_max = count($map[0]);
     $y_max = count($map);
 
+    $inf = 100000;
+
     $risks = $map;
     for ($y = 0; $y < $y_max; $y++) {
         for ($x = 0; $x < $x_max; $x++) {
-            $risks[$y][$x] = 100000;
+            $risks[$y][$x] = $inf;
         }
     }
 
     $map[$start_y][$start_x] = 0;
     $risks[$start_y][$start_x] = 0;
 
-    $changed = true;
-    while($changed) {
-        $queue = [[$start_x,$start_y]];
-        $visited = [];
-        $changed = false;
+    $queue = new PriorityQueue();
+    $queue->allocate(4096);
+    $queue->push([$start_x,$start_y],0);
+    $visited = [];
 
-        for ($i = 0; $i < count($queue); $i++) {
-            [$x, $y] = $queue[$i];
-            $key = $x . ',' . $y;
-            if (array_key_exists($key, $visited))
-                continue;
-            $visited[$key] = 1;
+    while (!$queue->isEmpty()) {
+        [$x, $y] = $queue->pop();
+        $key = $x . ',' . $y;
+        if (array_key_exists($key, $visited))
+            continue;
+        $visited[$key] = 1;
 
-            $adj_risks = [[$x + 1, $y], [$x, $y + 1], [$x - 1, $y], [$x, $y - 1]];
-            foreach ($adj_risks as $adj_risk) {
-                if ($adj_risk[0] < $x_max && $adj_risk[0] >= 0 && $adj_risk[1] < $y_max && $adj_risk[1] >= 0 ) {
-                    $risk = $map[$adj_risk[1]][$adj_risk[0]] + $risks[$y][$x];
-                    if ($risk < $risks[$adj_risk[1]][$adj_risk[0]]) {
-                        $risks[$adj_risk[1]][$adj_risk[0]] = $risk;
-                        $changed = true;
-                    }
-                    if(!(array_key_exists($adj_risk[0] . ',' . $adj_risk[1], $visited)))
-                        array_push($queue, [$adj_risk[0], $adj_risk[1]]);
-                }
+        $adj_risks = [[$x + 1, $y], [$x, $y + 1], [$x - 1, $y], [$x, $y - 1]];
+        foreach ($adj_risks as $adj_risk) {
+            if ($adj_risk[0] < $x_max && $adj_risk[0] >= 0 && $adj_risk[1] < $y_max && $adj_risk[1] >= 0 ) {
+                $risk = $map[$adj_risk[1]][$adj_risk[0]] + $risks[$y][$x];
+                if ($risk < $risks[$adj_risk[1]][$adj_risk[0]])
+                    $risks[$adj_risk[1]][$adj_risk[0]] = $risk;
+
+                if(!(array_key_exists($adj_risk[0] . ',' . $adj_risk[1], $visited)))
+                    $queue->push([$adj_risk[0], $adj_risk[1]], $inf-$risk);
             }
-
-
         }
     }
     return $risks[$stop_y][$stop_x];
 }
 
+
 function part1($map): int
 {
-    return find_lowest_total_risk(0,0, count($map[0]) - 1 , count($map) - 1, $map);
+    return find_lowest_total_risk_pq(0,0, count($map[0]) - 1 , count($map) - 1, $map);
 }
 
 function part2($map): int
@@ -98,9 +97,8 @@ function part2($map): int
             array_push($map2, $temp);
         }
     }
-    return find_lowest_total_risk(0,0, count($map2[0]) - 1 , count($map2) - 1, $map2);
+    return find_lowest_total_risk_pq(0,0, count($map2[0]) - 1 , count($map2) - 1, $map2);
 }
-
 $map= parse_input();
 echo "Part 1: " . part1($map) . PHP_EOL;
 echo "Part 2: " . part2($map) . PHP_EOL;
